64f911ac7fbb0f959b3f0012192b6119d45c801a
Andy Edwards <jedwards@fastmail.com>
2023-11-09 13:21:38 -0600

    fix: fix featureStatesReducer TS type

--- src/featureStatesReducer.d.ts
+++ src/featureStatesReducer.d.ts
@@ -1,6 +1,6 @@
-import type { Reducer } from 'redux'
-import type { FeatureStates, CreateReducer, FeatureAction } from './index'
+import type { AnyAction, Reducer } from 'redux'
+import type { FeatureStates, CreateReducer } from './index'
 export default function featureStatesReducer<
   S extends FeatureStates = FeatureStates,
-  A extends FeatureAction = FeatureAction
+  A extends AnyAction = AnyAction
 >(config?: { createReducer?: CreateReducer<S, A> }): Reducer<S, A>
